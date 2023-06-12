96ca0f07ed59ace69aa7660a411d1920d9d22c87
Chrishan Perera <pererachrishan7@gmail.com>
2023-06-12 20:09:01 +0930

    JSX, Components, Props

--- my-react-tutorial-1/src/App.js
+++ my-react-tutorial-1/src/App.js
@@ -4,9 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      { "Hello World!"}
+      <Job salary={ 9000} position="SE" company="apple" />
+      <Job salary={6000} position="QA" company="tesla" />
+      <Job salary={ 3000} position="HR" company="amazon" />
     </div>
   );
 }
+
+const Job = (props) => { 
+  return (
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
+    </div>
+  );
+};
+
 
 export default App;
